64c3d9e54d0da579b805f8166a9eedde390fbd27
whisper <cndone@gmail.com>
2017-04-16 23:17:48 +0900

    master/detail
    baseから angular cliへ移す

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
-import { browser, element, by } from 'protractor';
+import { Angular2demoPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('angular2demo App', () => {
+  let page: Angular2demoPage;
 
-  let expectedMsg = 'Hello Angular';
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new Angular2demoPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
-
 });
