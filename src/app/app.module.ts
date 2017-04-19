3c1073b6e5531a2bed9edc0afc712717bf6ae01e
whisper <cndone@gmail.com>
2017-04-20 00:25:04 +0900

    router対応

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,47 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }  from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    //    RouterModule.forRoot([{
+    //      path: '',
+    //      redirectTo: '/dashboard',
+    //      pathMatch: 'full'
+    //    },
+    //    {
+    //      path: 'heroes',
+    //      component: HeroesComponent
+    //    }, {
+    //      path: 'dashboard',
+    //      component: DashboardComponent
+    //    }, {
+    //      path: 'detail/:id',
+    //      component: HeroDetailComponent
+    //    }
+    //    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
-  bootstrap: [ AppComponent ]
+  providers: [
+    HeroService
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
